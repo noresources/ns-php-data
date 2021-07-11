d15adfa73e5de48d839ea52ecd61bbd96a458657
Renaud Guillard <dev@nore.fr>
2021-07-12 01:34:39 +0200

    github tests debug (bis)

--- tests/Serialization/SerializerTest.php
+++ tests/Serialization/SerializerTest.php
@@ -258,8 +258,10 @@ final class SerializerTest extends \PHPUnit\Framework\TestCase
 
 			$this->assertTrue(
 				$serializer->canUnserializeFromFile($filename),
-				$x . ' support (' . \basename($filename) . ' ' .
-				\strval(MediaTypeFactory::fromMedia($filename)) . ')');
+				\basename($filename) . ' ' .
+				\strval(MediaTypeFactory::fromMedia($filename)) . ' ' .
+				$x . ' support using ' .
+				TypeDescription::getLocalName($classname));
 
 			$actual = $serializer->unserializeFromFile($filename);
 
